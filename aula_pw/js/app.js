6eef8fd9d52f38e783565455ca827506f81ae8ff
CaiqueeLisboa <Caique.santana@etec.sp.gov.br>
2022-03-15 20:42:16 -0300

    terminando a função de pesquisa

--- aula_pw/js/app.js
+++ aula_pw/js/app.js
@@ -1,61 +1,116 @@
+/* função que inicia o body com o resultado de pesquisa dos cards*/
 function preRender(){
-    let countInvisibleCard = getCountInvisibleCards()
-    updateResults(countInvisibleCard)
+    let countVisibleCards = getCountVisibleCards();
+    updateResults(countVisibleCards);
 }
 
-/* Conta a quantidade de cards com nome diferente de "none"*/
-function getCountInvisibleCards(){
-    return Array.from(document.getElementsByClassName("card")).filter((card ) => !card.getElementsByClassName.display || card.getElementsByClassName !== "none").length
+/* faz a contagem de quantos cards são visiveis */
+function getCountVisibleCards(){
+    return Array.from(document.getElementsByClassName("card")).filter((card) => !card.getElementsByClassName.display || card.getElementsByClassName.display !=="none").length;
 }
 
+/* Atualiza o resultado dos cards */
 function updateResults(count) {
     document.getElementById("countResult").textContent = count;
 }
 
-function filter(){
-    let {search, operation, language} = getFilterProperties();
-
+/* Função que faz a pesquisa no container */
+function filter() {
+    let {search, operation, languages} = getFilterProperties();
     let interval = setInterval((_) => {
         let[containerEl] = document.getElementsByClassName("container");
         let changedText = search !== getSearchValue();
         if(!changedText) clearInterval(interval);
-        if(containerEl && containerEl.children && !changedText){
-            let visibleCards = updateVisibleCards(containerEl,search, operation, languages);
+        if(containerEl && containerEl.children && !changedText) {
+            let visibleCards = updateVisibleCards(containerEl,search,operation, languages);
+            updateResults(visibleCards);
         }
-    },10000);
+    }, 1000);
 }
 
-function getFilterProperties(){
+/* Função que armazena o que foi feito nos campos de pesquisa */
+function getFilterProperties() {
     let search = getSearchValue();
-    let [radio] = getSelectRadio();
-    /*Se o valor de radio for 1 faz uma operação de and se não faz uma operação de Or */
-    let operation = radio.id == "1 " ? "AND" : "OR";
-    /*Retorna uma array pra variavle language com o resultado da função getSelectLanguages */
-    let languages = Array.from(getSelectLanguages()).map((lang) => lang.name);
+    let[radio] = getSelectedRadio();
+    let operation = radio.id == "1" ? "AND" : "OR";
+    let languages = Array.from(getSelectedLanguages()).map((lang) => lang.name);
     return {
         search,
         operation,
-        languages
+        languages,
     }
 }
 
-/*Retorna o valor da pesquisa no input nameSearch*/
+/* Função que armazena oq foi digitado no campo de busca por nome */
 function getSearchValue(){
     let inputSearchEl = document.getElementById("nameSearch");
     return inputSearchEl.value;
 }
 
-/*Retorna o valor do input radio que estiver marcado */
-function getSelectRadio() {
+function getSelectedRadio(){
     return Array.from(document.querySelectorAll('header input[type="radio"]:checked'));
 }
 
-/*Retorna o valor do input checkbox que estiver marcado */
-function getSelectLanguages(){
-    return Array.from(document.querySelectorAll('header input[type="checkbox]:checked'));
+/* Função que armazena oq foi selecionado no campo por linguagem */
+function getSelectedLanguages(){
+    return Array.from(document.querySelectorAll('header input[type="checkbox"]:checked'));
 }
 
-function updateVisibleCards(containerEl, search, operation, languages) {
+/* Função que faz a validação dos cards baseado nas pesquisas */
+function updateVisibleCards(containerEl, search, operation, selectedLanguages){
     let visibleCards = 0;
-    Array.from();
+    /* percorre o container */
+    Array.from(containerEl.children).forEach((cardEl) => {
+        /* recebe o nome do titulo do card */
+        let [titleEl] = cardEl.getElementsByClassName("card-title");
+        /* recebe as linguagens do card */
+        let cardLanguages = Array.from(cardEl.getElementsByClassName("iconLanguage")).map((image) => image.name);
+        /* Se existir um tilte no card ele começa as validações */
+        if(titleEl) {
+            let isMatchName = isMatchByName(titleEl.textContent, search);
+            if(!isMatchName && operation == "AND"){
+                hideCard(cardEl);
+            } else if(isMatchName && operation == "OR") {
+                showCard(cardEl);
+                visibleCards++;
+            } else if(isMatchName && operation == "AND"){
+                let isMatchLanguage = isMatchByLanguage(cardLanguages, selectedLanguages);
+                if(isMatchLanguage) {
+                    showCard(cardEl);
+                    visibleCards++;
+                } else{
+                    hideCard(cardEl);
+                }
+            } else if (!isMatchName && operation == "OR") {
+                let isMatchLanguage = isMatchByLanguage(cardLanguages, selectedLanguages);
+                if(isMatchLanguage){
+                    showCard(cardEl);
+                    visibleCards++;
+                } else {
+                    hideCard(cardEl);
+                }
+            }
+        }
+    });
+    return visibleCards;
+}
+
+/* função que recebe oq foi digitado para a validação */
+function isMatchByName(textCard, textInput) {
+    return textCard.toLowerCase().includes(textInput.toLowerCase());
+}
+
+/* função que recebe oq foi selecionado para a validação */
+function isMatchByLanguage(cardLanguages, selectedLanguages){
+    return cardLanguages.some(cardLang => selectedLanguages.includes(cardLang));
+}
+
+/* função que esconde o card */
+function hideCard(card) {
+    card.style.display = "none";
+};
+
+/* função que mostra o card */
+function showCard(card) {
+    card.style.display = "flex";
 }
