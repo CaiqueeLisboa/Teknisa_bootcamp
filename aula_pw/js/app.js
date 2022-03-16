0d3d22115a029dfc0aa21978e5b553d10694f998
CaiqueeLisboa <Caique.santana@etec.sp.gov.br>
2022-03-15 21:09:45 -0300

    criando a função modal nos cards

--- aula_pw/js/app.js
+++ aula_pw/js/app.js
@@ -114,3 +114,61 @@ function hideCard(card) {
 function showCard(card) {
     card.style.display = "flex";
 }
+
+/* Constante o html do modal */
+const modalTemplate = `
+      <div class="modal">
+        <button class="fechar">x</button>
+        <div class="profilePictureModal">
+            <img class="image" name ="image-profile" src="__DEV_IMAGE__" alt="desenvolvedor" />
+        </div>
+        <div class="profileDescriptionModal">
+            <div style="display: flex; flex-direction: column">
+                <h2 class="card-title" name="devname">__DEV_NAME__</h2>
+                <span name="age">__DEV_AGE__</span>
+                <span name="description">__DEV_DESCRIPTION__</span>
+                <span name="description">__DEV_DESCRIPTION_ABILITY__</span>
+                <h3>Contato</h3>
+                <span name="mail">Email: <a href="#">__DEV_MAIL__</a></span>
+                <span name="git">Github: <a href="#">__DEV_GIT__</a></span>
+                <span name="phone">Telefone:__DEV_PHONE__</span>
+            </div>
+            <div class="languages">
+            <h3>Linguagens</h3>
+                __DEV_LANGUAGES__
+            </div>
+        </div>
+      </div>
+`;
+
+/*Percorre os cards para adicionar a função de click para abrir o modal*/
+Array.from(document.querySelectorAll('.card')).forEach(card => {
+    card.addEventListener('click', () => iniciaModal('modal-profile', event.currentTarget.id));
+});
+
+
+/* Função que faz funcionar o click para abrir o modal */
+function iniciaModal(modalId, cardId) {
+    fillModal(getUserInfo(cardId));
+    const modal = document.getElementById(modalId);
+    if(modal) {
+        modal.classList.add("mostrar");
+        modal.addEventListener("click", (e) => {
+            if(e.target.id == modalId || e.target.className == "fechar"){
+                modal.classList.remove("mostrar");
+            }
+        })
+    }
+}
+
+/* Função que pega as informações dos cards e adiciona em um objeto*/
+function getUserInfo(id){
+    let cardUser = document.getElementById(id);
+    let userData = {};
+    if(cardUser) {
+        userData = ['age', 'mail', 'phone', 'github', 'username', 'description', 'descriptionAbility'].reduce((acc,name) => {
+            acc[name] = getTextContentByName(cardUser,name);
+            return acc;
+        }, {});
+    }
+}
